cd7c2130f91731495c90cf1a6fe422d8741fa8e7
17038634 <17038634@student.hhs.nl>
2021-01-28 17:42:14 +0100

    Final

---
+++ PD-16/CoronaDapp/contracts/CoronaTest.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.0;
+pragma experimental ABIEncoderV2;
+
+import './Patient.sol';
+
+contract CoronaTest {
+    
+    constructor () {}
+    
+    function coronaTest(Patient memory _patient) public pure returns (bool result){
+      require(keccak256(abi.encodePacked(_patient.patientName)) != keccak256(abi.encodePacked('')), "Patient not found");
+        if (_patient.patientAge >= 18 && _patient.hasFever == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
